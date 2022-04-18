e6ec3e80125595008d0abb3dd0b2776b0ba0fad6
AxiosDeminence <juhmer@att.net>
2022-04-17 20:14:20 -0700

    Refactor crawling method

--- src/module.js
+++ src/module.js
@@ -1,95 +1,71 @@
 // htmlparser2 does not fix malformed html. The website we have
 // uses a lot of malformed html including no tbody or anything.
+// However, it does have a performance benefit since we do not need to fixup
+// the malformed html but would require more processing in the longterm.
 // const htmlparser2 = require('htmlparser2');
 const cheerio = require('cheerio');
 const fs = require('fs');
 
-async function crawl(id) {
-  async function getRollById(id) {
-    const form_details = {
-      name: id,
-      search: 'Search ID!',
-    };
+const id_query_url = 'http://cydel.net/idlook.php';
+const character_query_url = 'http://cydel.net/dicelook.php';
 
-    const response = await fetch('http://cydel.net/idlook.php', {
-      method: 'POST',
-      headers: {
-        'Accept-Encoding': 'gzip, deflate',
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: new URLSearchParams(form_details),
-    });
-    const response_text = await response.text();
-    return response_text;
+const character_selector =
+  'body > center > table:nth-child(3) > tbody > tr:nth-child(2) > ' +
+  'td:nth-child(6)';
+
+const headers = {
+  'Accept-Encoding': 'gzip, deflate',
+  'Content-Type': 'application/x-www-form-urlencoded',
+};
+
+async function queryRolls(type, key) {
+  let url, request_body;
+  switch (type) {
+    case 'id':
+      url = id_query_url;
+      request_body = {
+        name: key,
+        search: 'Search ID!',
+      };
+      break;
+    case 'character':
+      url = character_query_url;
+      request_body = {
+        name: key,
+        'lets roll!': 'Search!',
+      };
+      break;
+    default:
+      throw TypeError(`Type ${type} not expected`);
   }
 
-  async function getRollsByCharacter(character = '') {
-    const form_details = {
-      name: character,
-      'lets rolls!': 'Search!',
-    };
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: headers,
+    body: new URLSearchParams(request_body),
+  });
+  const response_text = await response.text();
+  return response_text;
+}
 
-    const response = await fetch('http://cydel.net/dicelook.php', {
-      method: 'POST',
-      headers: {
-        'Accept-Encoding': 'gzip, deflate',
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: new URLSearchParams(form_details),
-    });
-    const response_text = await response.text();
-    return response_text;
-  }
+function extractCharacterFromIDQuery(response) {
+  const $ = cheerio.load(response);
+  return {
+    character_name: $(character_selector).html(),
+    tag_in_name: $(character_selector).get(0).childNodes.length == 2,
+  };
+}
 
-  let html_response;
-  let dom;
-  try {
-    html_response = await getRollById(id);
-    // dom = htmlparser2.parseDocument(html_response);
-  } catch (e) {
-    console.error('Error: ', e);
-    /* Done to propogate fetch errors */
-    throw `Error when fetching roll with ID ${id}`;
-  }
-
-  let $ = cheerio.load(html_response);
-
-  // const id_selector =
-  //   'body > center > table > table:nth-child(4) > tr > ' +
-  //   'td:nth-child(6)';
-  const id_selector =
-    'body > center:nth-child(1) > table:nth-child(3) > tbody:nth-child(1) > ' +
-    'tr:nth-child(2) > td:nth-child(6)';
-
-  /* Character will be undefined if no roll with ID was found*/
-  const character = $(id_selector).html();
-  let tag_included = $(id_selector).get(0).childNodes.length == 2;
-
-  if (typeof character === 'undefined') {
-    throw `Error: Roll with ID ${id} not found.`;
-  }
-
-  try {
-    html_response = await getRollsByCharacter(character);
-    // dom = htmlparser2.parseDocument(html_response);
-  } catch (e) {
-    console.error('Error: ', e);
-    throw `Error when fetching rolls with character ${character}`;
-  }
-
-  // The responses are pretty malformed with single quoted attributes
-  // and unclosed elements. There should technically be two tables but in the
-  // raw response but it is not closed and cheerios does not pick it up.
-  // const table_selector =
-  //   'body > center > table > table:nth-child(4) > tr';
-
+function createTSVFromCharacterQuery(response, tag_in_name) {
   const table_selector =
-    'body > center:nth-child(1) > table:nth-child(' +
-    (tag_included + 3) +
-    ') > tbody:nth-child(1) > tr:not(:nth-child(1))';
+    'body > center > table:nth-child(' +
+    (tag_in_name + 3) +
+    ') > tbody > ' +
+    'tr:not(:nth-child(1))';
 
-  $ = cheerio.load(html_response);
+  const $ = cheerio.load(response);
   let tsv = '';
+  console.log($('body > center > table:nth-child(3) > tbody').html());
   $(table_selector)
     .children('td')
     .each((idx, elem) => {
@@ -123,4 +99,39 @@ async function crawl(id) {
   return tsv;
 }
 
-crawl(1);
+async function crawl(id) {
+  let html_response;
+  try {
+    html_response = await queryRolls('id', id);
+  } catch (e) {
+    console.error('Error: ', e);
+    /* Done to propogate fetch errors */
+    throw `Error when fetching roll with ID ${id}`;
+  }
+
+  const id_query_results = extractCharacterFromIDQuery(html_response);
+
+  /* Character will be undefined if no roll with ID was found*/
+  if (typeof id_query_results.character_name === 'undefined') {
+    throw `Error: Roll with ID ${id} not found.`;
+  }
+
+  try {
+    html_response = await queryRolls(
+      'character',
+      id_query_results.character_name
+    );
+  } catch (e) {
+    console.error('Error: ', e);
+    throw `Error when fetching rolls with character ${character}`;
+  }
+
+  return createTSVFromCharacterQuery(
+    html_response,
+    id_query_results.tag_in_name
+  );
+}
+
+crawl(1).then((tsv) => {
+  console.log(tsv);
+});
