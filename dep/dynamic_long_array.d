fb7f1cd79cca16d32c08363c2e4a929ea3cce552
AxiosDeminence <juhmer@att.net>
2022-04-23 13:33:55 -0700

    Ignore dependency directory

--- dep/dynamic_long_array.d
+++
@@ -1,4 +0,0 @@
-build/dynamic_long_array.o: src/dynamic_long_array.c \
- include/dynamic_long_array.h
-
-include/dynamic_long_array.h:
--- dep/main.d
+++
@@ -1,8 +0,0 @@
-build/main.o: src/main.c include/dynamic_long_array.h \
- include/missing_id.h include/dynamic_long_array.h
-
-include/dynamic_long_array.h:
-
-include/missing_id.h:
-
-include/dynamic_long_array.h:
